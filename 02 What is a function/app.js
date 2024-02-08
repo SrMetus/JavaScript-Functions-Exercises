55de820d42082a59e3ee2f189e7d767de2da7cc7
SrMetus <s.gaggero87@gmail.com>
2024-02-07 22:23:01 -0300

    Adding exercises 1 to 4

---
+++ 02 What is a function/app.js
@@ -0,0 +1,6 @@
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+// Your code here
+let superDuper = sum(3445324, 53454423)
---
+++ 04 Defining vs Calling a function/app.js
@@ -0,0 +1,9 @@
+// Define the function called "multi" that expects 2 parameters:
+function multi(num1, num2) {
+    let returnValue = num1 * num2;
+    return returnValue;
+}
+
+// Don't edit anything below this line
+let returnValue = multi(7, 53812212);
+console.log(returnValue);
